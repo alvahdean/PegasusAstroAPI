c72e26777ae8881880cdc92fd5ddab5bd49c39c2
Dean Fuqua <alvahdean@gmail.com>
2022-02-24 22:39:21 -0500

    Add test console program

---
+++ PegasusTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using PegasusDriver;
+using System;
+using System.Linq;
+using System.Runtime.Versioning;
+
+namespace PegasusTest
+{
+    [UnsupportedOSPlatform("android")]
+    [UnsupportedOSPlatform("ios")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var device = new PocketPowerBoxDriver();
+
+            //Console.WriteLine("Scanning COM Ports...");
+            //foreach(var portNportame in portNames)
+            //{
+            //    Console.WriteLine($"\t[{portName}]");
+            //}
+            string portName;
+
+            if (args.Count() > 0)
+            {
+                portName = args[0];
+            }
+            else
+            {
+                portName = device.GetPorts().FirstOrDefault();
+            }
+
+            if(string.IsNullOrWhiteSpace(portName))
+            {
+                Console.WriteLine("Not port found or specified");
+                return;
+            }
+
+            Console.WriteLine($"Connecting to {portName}");
+            device.Connect(portName);
+            var isOk = device.Ping();
+
+            if(!isOk)
+            {
+                Console.WriteLine($"Device is not repsonding, disconnecting");
+                device.Disconnect();
+                return;
+            }
+
+            Console.WriteLine($"Device is Ok!");
+            
+            Console.WriteLine($"Disconnecting...");
+            device.Disconnect();
+
+            Console.WriteLine($"Done.");
+        }
+    }
+}
